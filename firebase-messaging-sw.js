cbfa20211a8d61f88e5dad4eb84b681bd2ef9248
miyamoto <miyamiya2013g@gmail.com>
2020-03-06 14:25:40 +0900

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,10 +1,7 @@
 let baseURL = '/';
-
 self.addEventListener('push', event => {
     let json = event.data.json();
-
     baseURL = json.data.url;
-
     event.waitUntil(
         self.registration.showNotification(json.notification.title, {
             'body': json.notification.body,
@@ -13,15 +10,7 @@ self.addEventListener('push', event => {
         })
     );
 });
-
 self.addEventListener('notificationclick', event => {
-    if (event.action === 'select-a') {
-        self.clients.openWindow('https://www.mitsue.co.jp/knowledge/blog/');
-    } else if (event.action === 'select-b') {
-        self.clients.openWindow('https://www.mitsue.co.jp/knowledge/blog/frontend/');
-    } else {
-        self.clients.openWindow(baseURL);
-    }
-
+    self.clients.openWindow(baseURL);
     event.notification.close();
 });
