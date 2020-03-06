dc20093cabe3c9b01fbbf1238d1e1558ddd9452c
miyamoto <miyamiya2013g@gmail.com>
2020-03-06 11:45:51 +0900

    update

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const messaging = firebase.messaging();
+console.log(messaging);
 messaging.usePublicVapidKey("BFAaREMWt7fx1KXI34GTC23oC7Qy8LZsY2DyN5O20HpV29J1WqnbCvaZPLiQRii9mw0lNycbUcC5inNy523WG1Y");
 messaging.onMessage(function(payload) {
 	console.log('Message received. ', payload);
