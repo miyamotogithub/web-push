bb27b340abd96df8ebe20f65beafb56c57823d35
miyamoto <miyamiya2013g@gmail.com>
2020-03-06 10:43:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -22,8 +22,26 @@ function init() {
     Notification.requestPermission().then((permission) => {
         if (permission === 'granted') {
             console.log('Notification permission granted.');
-            // TODO(developer): Retrieve an Instance ID token for use with FCM.
-            // ...
+            // Get Instance ID token. Initially this makes a network call, once retrieved
+            // subsequent calls to getToken will return from cache.
+            messaging.getToken().then((currentToken) => {
+                if (currentToken) {
+                /*sendTokenToServer(currentToken);
+                updateUIForPushEnabled(currentToken);*/
+                console.log(currentToken);
+                } else {
+                // Show permission request.
+                console.log('No Instance ID token available. Request permission to generate one.');
+                // Show permission UI.
+                /*updateUIForPushPermissionRequired();
+                setTokenSentToServer(false);*/
+                }
+            }).catch((err) => {
+                console.log('An error occurred while retrieving token. ', err);
+                /*showToken('Error retrieving Instance ID token. ', err);
+                setTokenSentToServer(false);*/
+            });
+
         } else {
             console.log('Unable to get permission to notify.');
         }
