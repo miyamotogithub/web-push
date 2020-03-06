fd270e7b12183a1d56b57f31f101dde91675cfce
miyamoto <miyamiya2013g@gmail.com>
2020-03-06 13:43:31 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,12 +8,12 @@ messaging.onMessage(function(payload) {
 	//appendMessage(payload);
 	// [END_EXCLUDE]
 });
-navigator.serviceWorker.register('./firebase-messaging-sw.js')
+/*navigator.serviceWorker.register('./firebase-messaging-sw.js')
 .then(function(registration) {
     console.log('Registration successful, scope is:', registration.scope);
 }).catch(function(err) {
     console.log('Service worker registration failed, error:', err);
-});
+});*/
 function requestPermission() {
     Notification.requestPermission()
     .then(function(permission) {
