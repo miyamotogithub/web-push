081f20b21fa5d0997a4e6bdece4eaddacbb8398a
miyamoto <miyamiya2013g@gmail.com>
2020-03-09 12:12:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const messaging = firebase.messaging();
 console.log(messaging);
-messaging.usePublicVapidKey("BFAaREMWt7fx1KXI34GTC23oC7Qy8LZsY2DyN5O20HpV29J1WqnbCvaZPLiQRii9mw0lNycbUcC5inNy523WG1Y");
+//messaging.usePublicVapidKey("BFAaREMWt7fx1KXI34GTC23oC7Qy8LZsY2DyN5O20HpV29J1WqnbCvaZPLiQRii9mw0lNycbUcC5inNy523WG1Y");
 messaging.onMessage(function(payload) {
 	console.log('Message received. ', payload);
 	// [START_EXCLUDE]
